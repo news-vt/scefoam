187d2d7682d9ac6f4ed8a4a3fa8ae2d29c4ac8dd
Jean-LucDeRieux <77759670+Jean-LucDeRieux@users.noreply.github.com>
2025-06-12 18:24:02 -0400

    wip: cleaned up repo

--- receiver/src/main.jsx
+++
@@ -1,7 +0,0 @@
-// src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Receiver from './Receiver';
-
-const rootElement = document.getElementById('root');
-createRoot(rootElement).render(<Receiver />);
--- transmitter/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the '/client' import
-import Transmitter from './Transmitter';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Transmitter />);
